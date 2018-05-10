93ed58c01c332d38c4269db51dd3906d22160686
Julian Sandersius <sandersiusjulian@gmail.com>
2018-05-09 17:52:24 -0700

    reconfiguring server.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -14,14 +14,17 @@ module.exports = function(app) {
 
     // index route loads view.html
 
-    
+
+//_________________
     app.get("/", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/blog.html"));
     });
 
+
     app.get("/cms", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/cms.html"));
     });
+
 
     // blog route loads blog.html
     app.get("/blog", function(req, res) {
@@ -29,6 +32,8 @@ module.exports = function(app) {
     });
 
 };
+
+//_____________
 
 // Routes
 // =============================================================
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var path = require("path");
 var Sequelize = require('sequelize');
 var mysql2 = require('mysql2')
 
+
 // Sets up the Express App
 // =============================================================
 var app = express();
@@ -19,8 +20,10 @@ var PORT = process.env.PORT || 3000;
 var db = require("./models/index.js");             //when inlcuding either of these lines, terminal reads "cannot find module sequalize"
 // var models = require('./models/sequelize');   
 
+
 // Static directory
 app.use(express.static("public"));
+
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
@@ -33,4 +36,94 @@ db.sequelize.sync({ force: true }).then(function() {
 
 
 
+// ____________________
 
+
+// *********************************************************************************
+// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+    // Each of the below routes just handles the HTML page that the user gets sent to.
+
+    // index route loads view.html
+
+
+//_________________
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/blog.html"));
+    });
+
+
+    app.get("/cms", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/cms.html"));
+    });
+
+
+    // blog route loads blog.html
+    app.get("/blog", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/blog.html"));
+    });
+
+};
+
+//_____________
+
+// Routes
+// =============================================================
+
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
+// Sets up the Express app to handle data parsing
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "test" }));
+
+app.set("view engine", "handlebars");
+
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./home.html"));
+});
+
+app.get("/userLogin", function(req, res) {
+
+    res.sendFile(path.join(__dirname, "./userLogin.html"));
+});
+
+app.get("/trainerLogin", function(req, res) {
+    res.sendFile(path.join(__dirname, "./trainerLogin.html"));
+});
+
+app.get("/contactUs", function(req, res) {
+    res.sendFile(path.join(__dirname, "./contactUs.html"));
+});
+
+app.get("/aboutUs", function(req, res) {
+    res.sendFile(path.join(__dirname, "./aboutUs.html"));
+});
+
+app.get("/userSubmit", function(req, res) {
+    res.sendFile(path.join(__dirname, "./userSubmit.html"));
+});
+
+app.get("/trainerSubmit", function(req, res) {
+    res.sendFile(path.join(__dirname, "./trainerSubmit.html"));
+});
+
+app.use(express.static('images'));
